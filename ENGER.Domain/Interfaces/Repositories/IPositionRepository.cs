a8631f1c468542b440b67f062c5df8350b4f535e
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2026-03-06 21:05:39 -0300

    feat: inserindo as interfaces e repositories

---
+++ ENGER.Domain/Interfaces/Repositories/IPositionRepository.cs
@@ -0,0 +1,17 @@
+﻿using ENGER.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENGER.Domain.Interfaces.Repositories
+{
+    public interface IPositionRepository
+    {
+        Task<int> AddAsync(Position position);
+        Task UpdateAsync(Position position);
+        Task<Position?> GetByIdAsync(int intPositionId);
+        Task<Position?> DeleteAsync(int intPositionId);
+    }
+}
---
+++ ENGER.Infrastructure/Repositories/PositionRepository.cs
@@ -0,0 +1,33 @@
+﻿using ENGER.Domain.Entities;
+using ENGER.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENGER.Infrastructure.Repositories
+{
+    public class PositionRepository : IPositionRepository
+    {
+        public Task<int> AddAsync(Position position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Position?> DeleteAsync(int intPositionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Position?> GetByIdAsync(int intPositionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Position position)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
